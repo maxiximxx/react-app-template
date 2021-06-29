3d949467d8a3f417dab78f53cf40debb321b58ae
mxx <474870484@qq.com>
2021-06-29 13:50:05 +0800

    优化

--- src/common/api/http.ts
+++ src/common/api/http.ts
@@ -1,23 +1,23 @@
 /* eslint-disable no-param-reassign */
-import axios from 'axios'
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
 
 const { CancelToken } = axios
 const cancelTokenMap = new Map()
 
-const generateCancelKey = (config) => {
+const generateCancelKey = (config: AxiosRequestConfig) => {
   const { url, method, params, data } = config
   const cancelKey = JSON.stringify({ url, method, params, data })
   return cancelKey
 }
 
-const addCancelRequest = (config) => {
+const addCancelRequest = (config: AxiosRequestConfig) => {
   const cancelKey = generateCancelKey(config)
   config.cancelToken = new CancelToken((cancel) => {
     cancelTokenMap.set(cancelKey, cancel)
   })
 }
 
-const removeCancelRequest = (config) => {
+const removeCancelRequest = (config: AxiosRequestConfig) => {
   const cancelKey = generateCancelKey(config)
   const cancel = cancelTokenMap.get(cancelKey)
   cancel && cancel(cancelKey)
@@ -50,7 +50,7 @@ axios.interceptors.response.use(
 
 // let requestErr = false
 
-const checkStatus = async (response) => {
+const checkStatus = async (response: AxiosResponse) => {
   // !requestErr && Toast.hide()
   if (response && response.status === 200) {
     if (response.data.code === 200) {
@@ -69,7 +69,7 @@ const checkStatus = async (response) => {
 }
 
 export default class http {
-  static post(url, data) {
+  static post(url: string, data?: object) {
     return axios({
       method: 'post',
       url,
@@ -78,7 +78,7 @@ export default class http {
     }).then((response) => checkStatus(response))
   }
 
-  static get(url, params) {
+  static get(url: string, params?: object) {
     return axios({
       method: 'get',
       url,
