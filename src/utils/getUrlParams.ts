35e7eb7adc7eba424dfe7818b847aef97b41ddab
mxx <474870484@qq.com>
2021-06-29 20:43:01 +0800

    优化代码

--- src/utils/getUrlParams.ts
+++ src/utils/getUrlParams.ts
@@ -4,8 +4,8 @@ interface paramsType {
   [key: string]: string
 }
 
-export default function getUrlParams() {
-  const searchParams = new URLSearchParams(window.location.search)
+export default function getUrlParams(url = window.location.search) {
+  const searchParams = new URLSearchParams(url)
   const params: paramsType = {}
   for (const [key, value] of searchParams) {
     params[key] = value
